96d66e9f9c4f858d5461127e622f48e7176df7ef
JDzul24 <jonidzul@gmail.com>
2023-11-01 18:31:53 -0600

    07 Crud de Vehiculos añadido - dev Jonathan

--- src/routes/vehiculos.js
+++ src/routes/vehiculos.js
@@ -2,5 +2,69 @@ const express = require('express');
 const router = express.Router();
 const ObjectId = require('mongoose').ObjectId;
 
+const Vechiculo = require('../models/VehiculosModel');
+
+router.get('/', async (req, res) => {
+    try {
+        const items = await Vechiculo.find();
+        res.json(items);
+        console.log(items)
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    const newItemData = req.body;
+
+    try {
+        const newItem = new Vechiculo(newItemData);
+        const result = await newItem.save();
+
+        if (result) {
+            res.status(201).json(result);
+        } 
+        else{
+            res.status(500).json({ error: 'Error en la inserción de datos' });
+        }
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const updatedItemData = req.body;
+    
+    try {
+      const result = await Cliente.findOneAndUpdate({ _id: ObjectId(req.params.id) }, updatedItemData, { new: true });
+      
+      if (result) {
+        res.json(result);
+      } else {
+        res.status(404).json({ error: 'Objeto no encontrado' });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+router.delete('/:id', async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const result = await Cliente.findOneAndRemove({ _id: ObjectId(userId) });
+
+    if (result) {
+      res.json({ message: 'El vehiculo fue eliminado' });
+    } else {
+      res.status(404).json({ error: 'Vehiculo no encontrado' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 
 module.exports = router;
